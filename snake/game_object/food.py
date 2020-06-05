0c8c75e65af3f2c66ac9ec1c487dd5c957d21e21
OkiDoki <Oleg130601@mail.ru>
2020-06-05 16:44:45 +0500

    commit object

--- snake/game_object/food.py
+++ snake/game_object/food.py
@@ -4,19 +4,36 @@ import random
 
 class Food:
     def __init__(self):
+        self.correct_list = [23, 34, 45, 56, 67, 78, 89, 100, 111, 122, 133, 144, 155, 166, 177, 188, 199, 210, 221,
+                             232, 243, 254, 265, 276, 287, 298, 309, 320, 331, 342, 353, 364, 375, 386, 397, 408]
 
-        self.food_position = []
-       # self.food_position[0] = 1
-       # self.food_position[1] = 1
+        self.food_position_x = 1
+        self.food_position_y = 1
+
+        self.yellow_food_pos_x = 1
+        self.yellow_food_pos_y = 1
+
+        self.blue_food_pos_x = 1
+        self.blue_food_pos_y = 1
 
     def get_food_position(self, gui):
-        """Выдает рандомное значение координат еды"""
-       # self.food_position = random.choice(gui.field)
-        self.food_position = [random.randint(1, 441) ,random.randint(1, 441)]
-
-       # self.food_position[0] = random.randint(6, 10)
-       # self.food_position[1] = random.randint(6, 100)
+        self.food_position_x = random.choice(self.correct_list)
+        self.food_position_y = random.choice(self.correct_list)
 
     def draw_food(self, window):
-        """отрисовка еды"""
-        pygame.draw.rect(window, pygame.Color("red"), pygame.Rect(self.food_position[0], self.food_position[1], 10,10))
+        pygame.draw.rect(window, pygame.Color("Red"), pygame.Rect(self.food_position_x, self.food_position_y, 10, 10))
+
+    def get_yellow_food_position(self, gui):
+        self.yellow_food_pos_x = random.choice(self.correct_list)
+        self.yellow_food_pos_y = random.choice(self.correct_list)
+
+    def draw_yellow_food(self, window):
+        pygame.draw.rect(window, pygame.Color("Yellow"),
+                         pygame.Rect(self.yellow_food_pos_x, self.yellow_food_pos_y, 10, 10))
+
+    def get_blue_food_position(self, gui):
+        self.blue_food_pos_x = random.choice(self.correct_list)
+        self.blue_food_pos_y = random.choice(self.correct_list)
+
+    def draw_blue_food(self, window):
+        pygame.draw.rect(window, pygame.Color("Blue"), pygame.Rect(self.blue_food_pos_x, self.blue_food_pos_y, 10, 10))
--- snake/game_object/snake.py
+++ snake/game_object/snake.py
@@ -3,16 +3,15 @@ import pygame
 
 class Snake:
     def __init__(self):
-        self.head = [45,45]
-        self.body = [[45,45], [34,45], [23,45]]
+        self.head = [45, 45]
+        self.body = [[45, 45], [34, 45], [23, 45]]
+        self.score = 0
 
     def draw_snake(self, window):
-        """Отрисовка змеи на экране"""
         for segment in self.body:
-            pygame.draw.rect(window, pygame.Color("green"), pygame.Rect(segment[0], segment[1], 10,10))
+            pygame.draw.rect(window, pygame.Color("green"), pygame.Rect(segment[0], segment[1], 10, 10))
 
     def moove(self, control):
-        """ Движение змеи в зависимости от направления"""
         if control.flag_direction == "RIGHT":
             self.head[0] += 11
         elif control.flag_direction == "LEFT":
@@ -23,12 +22,10 @@ class Snake:
             self.head[1] += 11
 
     def animation(self):
-        """прибавляем к началу списка тела голову, а хвост удаляем"""
         self.body.insert(0, list(self.head))
         self.body.pop()
 
     def check_end_window(self):
-        """Отслеживает достижение змеей края экрана"""
         if self.head[0] == 419:
             self.head[0] = 23
         elif self.head[0] == 12:
@@ -39,7 +36,31 @@ class Snake:
             self.head[1] = 34
 
     def eat(self, food, gui):
-        """Ест еду"""
-        if self.head == food.food_position:
-            self.body.append(food.food_position)
+        if self.head[0] == food.food_position_x and self.head[1] == food.food_position_y:
+            self.body.append([food.food_position_x, food.food_position_y])
+            gui.get_new_indicator()
             food.get_food_position(gui)
+            food.get_blue_food_position(gui)
+            food.get_yellow_food_position(gui)
+
+        elif self.head[0] == food.blue_food_pos_x and self.head[1] == food.blue_food_pos_y:
+            self.body.append([food.blue_food_pos_x, food.blue_food_pos_y])
+            food.get_food_position(gui)
+            food.get_blue_food_position(gui)
+            gui.get_new_indicator()
+            gui.get_new_indicator()
+
+        elif self.head[0] == food.yellow_food_pos_x and self.head[1] == food.yellow_food_pos_y:
+            self.body.pop()
+            gui.indicator.pop()
+            food.get_food_position(gui)
+            food.get_blue_food_position(gui)
+            food.get_yellow_food_position(gui)
+
+    def check_barriers(self, gui):
+        if self.head in gui.barrier:
+            self.body.pop()
+            gui.indicator.pop()
+        if self.head in self.body[1:]:
+            self.body.pop()
+            gui.indicator.pop()
