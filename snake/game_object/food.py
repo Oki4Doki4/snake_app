f8c405d7267ed833f5ee54e3e041e27a2e5dc943
OkiDoki <Oleg130601@mail.ru>
2020-06-08 12:17:18 +0500

    update food

--- snake/game_object/food.py
+++ snake/game_object/food.py
@@ -2,38 +2,22 @@ import pygame
 import random
 
 
-class Food:
-    def __init__(self):
-        self.correct_list = [23, 34, 45, 56, 67, 78, 89, 100, 111, 122, 133, 144, 155, 166, 177, 188, 199, 210, 221,
-                             232, 243, 254, 265, 276, 287, 298, 309, 320, 331, 342, 353, 364, 375, 386, 397, 408]
+class Food():
+    def __init__(self, food_color, screen_width, screen_height):
+        self.food_color = food_color
+        self.food_size_x = 10
+        self.food_size_y = 10
+        self.food_pos = [random.randrange(1, screen_width / 10) * 10,
+                         random.randrange(1, screen_height / 10) * 10]
+        self.food_yellow_pos = [random.randrange(1, screen_width / 10) * 10,
+                                random.randrange(1, screen_height / 10) * 10]
 
-        self.food_position_x = 1
-        self.food_position_y = 1
+    def draw_food(self, play_surface):
+        pygame.draw.rect(play_surface, self.food_color, pygame.Rect(
+            self.food_pos[0], self.food_pos[1],
+            self.food_size_x, self.food_size_y))
 
-        self.yellow_food_pos_x = 1
-        self.yellow_food_pos_y = 1
-
-        self.blue_food_pos_x = 1
-        self.blue_food_pos_y = 1
-
-    def get_food_position(self, gui):
-        self.food_position_x = random.choice(self.correct_list)
-        self.food_position_y = random.choice(self.correct_list)
-
-    def draw_food(self, window):
-        pygame.draw.rect(window, pygame.Color("Red"), pygame.Rect(self.food_position_x, self.food_position_y, 10, 10))
-
-    def get_yellow_food_position(self, gui):
-        self.yellow_food_pos_x = random.choice(self.correct_list)
-        self.yellow_food_pos_y = random.choice(self.correct_list)
-
-    def draw_yellow_food(self, window):
-        pygame.draw.rect(window, pygame.Color("Yellow"),
-                         pygame.Rect(self.yellow_food_pos_x, self.yellow_food_pos_y, 10, 10))
-
-    def get_blue_food_position(self, gui):
-        self.blue_food_pos_x = random.choice(self.correct_list)
-        self.blue_food_pos_y = random.choice(self.correct_list)
-
-    def draw_blue_food(self, window):
-        pygame.draw.rect(window, pygame.Color("Blue"), pygame.Rect(self.blue_food_pos_x, self.blue_food_pos_y, 10, 10))
+    def draw_yellow_food(self, play_surface):
+        pygame.draw.rect(play_surface, pygame.Color("Yellow"), pygame.Rect(
+            self.food_yellow_pos[0], self.food_yellow_pos[1],
+            self.food_size_x, self.food_size_y))
